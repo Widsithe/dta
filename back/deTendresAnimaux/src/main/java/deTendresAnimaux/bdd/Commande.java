98130e331c817984c2ce7aa14a95af00fa3cb5b9
Unknown <amimi.faycel@gmail.com>
2018-07-02 15:42:25 +0200

    [update] BDD
    creation de base de donnee avec hibernate

---
+++ back/deTendresAnimaux/src/main/java/deTendresAnimaux/bdd/Commande.java
@@ -0,0 +1,61 @@
+package deTendresAnimaux.bdd;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Commande {
+	@Id
+	@GeneratedValue
+	private Integer idCommande;
+	public Commande(LocalDate dateCommande) {
+		
+		this.dateCommande = dateCommande;
+	}
+
+	private LocalDate dateCommande;
+	@ManyToOne
+	private Client client;
+	
+   
+	@OneToMany(mappedBy = "commande")
+	private List<Quantite> commande;
+	
+	public Commande() {
+	}
+
+	public Commande(LocalDate dateCommande, Client client) {
+		this.dateCommande = dateCommande;
+		this.client = client;
+	}
+
+	public Integer getIdCommande() {
+		return idCommande;
+	}
+
+	public void setIdcommande(Integer idCommande) {
+		this.idCommande = idCommande;
+	}
+
+	public LocalDate getDateCommande() {
+		return dateCommande;
+	}
+
+	public void setDatecommande(LocalDate dateCommande) {
+		this.dateCommande = dateCommande;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+}
