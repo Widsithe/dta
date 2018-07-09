c076e9975bb75a6da8f99aef1de52fc94010d173
Vivien Delmas <viviendelmas@hotmail.com>
2018-07-10 00:16:03 +0200

    Auto stash before merge of "master" and "origin/master"
    [UPDATE] Security spring

---
+++ back/deTendresAnimaux/src/main/java/deTendresAnimaux/security/MyAccessDeniedHandler.java
@@ -0,0 +1,30 @@
+package deTendresAnimaux.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+
+	public MyAccessDeniedHandler() {
+	}
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		// do some business logic, then redirect to errorPage url
+
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		// ou redirect to an error page url
+
+	}
+
+}
---
+++ back/deTendresAnimaux/src/main/java/deTendresAnimaux/security/MyAuthenticationFailureHandler.java
@@ -0,0 +1,27 @@
+package deTendresAnimaux.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+	public MyAuthenticationFailureHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException exception) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		response.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED);
+	}
+
+}
