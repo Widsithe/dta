a5ceb9767ea043c899f8194951d752aa6aa348a2
Vivien Delmas <viviendelmas@hotmail.com>
2018-07-11 14:08:47 +0200

    [MOD] admin controller
    ??

--- back/deTendresAnimaux/src/main/java/deTendresAnimaux/controller/AdminController.java
+++ back/deTendresAnimaux/src/main/java/deTendresAnimaux/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 import deTendresAnimaux.bdd.Produit;
 import deTendresAnimaux.security.AuthenticationService;
 import deTendresAnimaux.service.AdminService;
+
+
+@CrossOrigin(origins = "http://localhost:4200/", maxAge = 3600)
 
 @RestController
 @RequestMapping("/api/admin/")
