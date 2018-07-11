899fd36d739f69d98ed62b9ef966a5c64fec91d8
Marine <marine.cabot@gmail.com>
2018-07-11 09:29:21 +0200

    [MOD] Form new user

--- front2/src/app/user/new-user/new-user.component.ts
+++ front2/src/app/user/new-user/new-user.component.ts
@@ -13,70 +13,59 @@ import { Router } from '@angular/router';
 })
 export class NewUserComponent implements OnInit {
   user: User;
-  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
   msgs: Message[] = [];
-  checkCGU: string;
-  CGUPanelVisi: boolean = false;
 
-  constructor(private userService: UserService,private router:Router) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
     this.resetForm();
   }
 
   resetForm(form?: NgForm) {
-    if (form != null)
+    if (form != null) {
       form.reset();
-    this.user = new User(0,"", "", "", "", null, "", "");
+    }
+    this.user = new User(0, '', '', '', '', null, '', '');
   }
 
   OnSubmit(form: NgForm) {
     this.msgs = [];
-    if (!this.checkCGU || this.checkCGU=="") {
-      this.msgs.push({
-        severity: 'error',
-        summary: "CGU / CGV",
-        detail: "Veuillez accepter les CGU et CGV afin de créer votre compte."
-      });
-      return;
-    }
+    return;
     if (!form.valid) {
       this.msgs.push({
         severity: 'error',
-        summary: "Champs vides",
-        detail: "Tous les champs de ce formulaire sont obligatoires."
+        summary: 'Champs vides',
+        detail: 'Tous les champs de ce formulaire sont obligatoires.'
       });
       return;
     }
-    //check phone input is valid
+    // tslint:disable-next-line:radix
     if (isNaN(parseInt(this.user.phone))) {
       this.msgs.push({
         severity: 'error',
-        summary: "Champ invalide",
+        summary: 'Champ invalide',
         detail: 'Vous devez saisir un numéro de téléphone'
       });
     } else {
       this.userService.registerUser(this.user)
         .subscribe((data: any) => {
-          if (data.state == "success") {
+          if (data.state === 'success') {
             this.resetForm(form);
             this.msgs.push({
               severity: 'success',
-              summary: "Votre compte a été créer",
+              summary: 'Votre compte a été créer',
               detail: 'Vous pouvez maintenant vous connecté'
             });
             this.router.navigate(['/authentification']);
           } else {
             this.msgs.push({
               severity: 'error',
-              summary: "Erreur",
+              summary: 'Erreur',
               detail: 'Cette addresse mail est déjà relié à un compte.'
             });
           }
         }, error => console.log(error));
     }
-  }
-  showCGU() {
-    this.CGUPanelVisi = true;
   }
 }
